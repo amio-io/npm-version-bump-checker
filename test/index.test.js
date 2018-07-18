5f61975315165540e357ec96e83e6c0f7edd7199
cybairfly <cyberfly@email.cz>
2018-07-18 14:50:12 +0200

    make tests more specific by including error message comparison on throw

--- test/index.test.js
+++ test/index.test.js
@@ -2,15 +2,27 @@ const validate = require('../src/validate')
 const expect = require('chai').expect
 
 describe('Validate updated package version', function () {
-  it('Validate an updated package version', async () => {
+  it('local version > remote version: validate ok', async () => {
     validate('1.2.3', '11.2.3')
   })
 
-  it('Throw error for lesser package version', async () => {
-    expect(validate.bind(null, '1.2.3', '0.12.3')).to.throw()
+  it('local version < remote version: throw error', async () => {
+    const versionLatest = '1.2.3'
+    const versionUpdate = '0.12.3'
+    expect(validate.bind(null, versionLatest, versionUpdate)).to.throw(`
+      Package version lesser than the version already published.
+      Remote package version: ${versionLatest}
+      Local package version: ${versionUpdate}
+    `)
   })
 
-  it('Throw error for identical package version', async () => {
-    expect(validate.bind(null, '1.2.3', '1.2.3')).to.throw()
+  it('local version = remote version: throw error', async () => {
+    const versionLatest = '1.2.3'
+    const versionUpdate = '1.2.3'
+    expect(validate.bind(null, versionLatest, versionUpdate)).to.throw(`
+      Package version identical to the version already published.
+      Remote package version: ${versionLatest}
+      Local package version: ${versionUpdate}
+    `)
   })
 })
